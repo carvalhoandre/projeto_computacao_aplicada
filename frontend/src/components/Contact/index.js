1386ad15a688866e696487d9dd251b5b9cf0b011
André Carvalho <66704150+carvalhoandre@users.noreply.github.com>
2022-04-25 20:37:06 -0300

    Update index.js

--- frontend/src/components/Contact/index.js
+++ frontend/src/components/Contact/index.js
@@ -77,7 +77,7 @@ export default function Contact() {
 
                 <section className="contact section" id="contactme">
                     <h2 className="section_title">Email Service</h2>
-                    <span className="section_subtitle">Cadastre-se eseja atualizado de nossas novidades</span>
+                    <span className="section_subtitle">Cadastre-se seja atualizado de nossas novidades</span>
 
                     <div className="contact_container container grid">
                         <div>
